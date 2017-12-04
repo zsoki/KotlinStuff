bc053a9d53a77d2dad91294dfd3e9ed42123653e
Zoltan Soki <v-zsoki@expedia.com>
2017-12-04 11:24:01 +0100

    Lazy examples

--- src/main/kotlin/hu/deadpool/kotlinstuff/Immutability.kt
+++ src/main/kotlin/hu/deadpool/kotlinstuff/Immutability.kt
@@ -1,6 +1,6 @@
 package hu.deadpool.kotlinstuff
 
-class Example {
+class ImmutabilityExample1 {
 
     lateinit var imLate: String
 
@@ -13,10 +13,25 @@ class Example {
 
 }
 
-class Lazy {
+class LazyExample {
 
-    val immaLazyPropertyBoi by lazy {
-        "heh."
-    }
+    val lazyVal1 by lazy { expensiveCalculation("ReturnString") }
+    val lazyVal2 by lazy { expensiveCalculation("ReturnString") }
+    val lazyVal3 by lazy { expensiveCalculation("ReturnString") }
+    val lazyVal4 by lazy { expensiveCalculation("ReturnString") }
 
+}
+
+class NotLazyExample {
+
+    val notLazyVal1 = expensiveCalculation("ReturnString")
+    val notLazyVal2 = expensiveCalculation("ReturnString")
+    val notLazyVal3 = expensiveCalculation("ReturnString")
+    val notLazyVal4 = expensiveCalculation("ReturnString")
+
+}
+
+private fun expensiveCalculation(stringToReturn: String) : String {
+    Thread.sleep(500)
+    return stringToReturn
 }
---
+++ src/test/kotlin/hu/deadpool/kotlinstuff/Immutability.kt
@@ -0,0 +1,62 @@
+package hu.deadpool.kotlinstuff
+
+import org.testng.annotations.Test
+import kotlin.system.measureTimeMillis
+
+class Immutability {
+
+    private val example = ImmutabilityExample1()
+
+    @Test
+    fun `lateinit variable is null`() {
+        assert(example.imLate == null)
+    }
+
+    @Test
+    fun `lateinit variable is not null`() {
+        assert(example.imLate != null)
+    }
+
+    @Test(expectedExceptions = [(UninitializedPropertyAccessException::class)])
+    fun `lateinit variable is neither null, nor not null, what is it then?`() {
+        example.nothinToSeeHereJustAccessinLength()
+    }
+
+    @Test
+    fun `measure construction time and val access WITHOUT lazy`() {
+        var notLazy: NotLazyExample? = null
+
+        val constructionTime = measureTimeMillis {
+            notLazy = NotLazyExample()
+        }
+
+        val accessTime = measureTimeMillis {
+            notLazy?.notLazyVal1
+            notLazy?.notLazyVal2
+            notLazy?.notLazyVal3
+            notLazy?.notLazyVal4
+        }
+
+        print("Construction in millis: $constructionTime")
+        print("Access in millis: $accessTime")
+    }
+
+    @Test
+    fun `measure construction time and val access WITH lazy`() {
+        var lazy: LazyExample? = null
+
+        val constructionTime = measureTimeMillis {
+            lazy = LazyExample()
+        }
+
+        val accessTime = measureTimeMillis {
+            lazy?.lazyVal1
+            lazy?.lazyVal2
+            lazy?.lazyVal3
+            lazy?.lazyVal4
+        }
+
+        print("Construction in millis: $constructionTime")
+        print("Access in millis: $accessTime")
+    }
+}
